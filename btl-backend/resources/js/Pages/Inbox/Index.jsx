a3776144904f477390804939e96750f0fe9344d4
XuanTruong <hdnk.it@gmail.com>
2023-12-14 16:55:25 +0700

    coding for inbox

--- btl-backend/resources/js/Pages/Inbox/Index.jsx
+++ btl-backend/resources/js/Pages/Inbox/Index.jsx
@@ -480,8 +480,21 @@ export default function Index({ auth, user}) {
                                     </div>
                                 </div>
                             </div>
-                            <div className="">
-                                <div className="">111</div>
+                            <div className="grow">
+                                <div className="flex flex-col justify-center h-full">
+                                    <div className="px-4 flex flex-col items-center">
+                                        <FontAwesomeIcon icon="fa-solid fa-inbox" className="text-[6em]" />
+                                    </div>
+                                    <div className="px-4 pt-5">
+                                        <span className="block text-xl leading-[25px] text-center">Your messages</span>
+                                    </div>
+                                    <div className="px-4 pt-4">
+                                        <span className="block text-sm leading-[18px] text-center text-[#737373]">Send private photos and messages to a friend or group</span>
+                                    </div>
+                                    <div className="px-4 pt-5 flex justify-center">
+                                        <button className="h-8 w-auto px-4 font-semibold rounded-lg text-sm text-white bg-[#0095F6] hover:bg-[#1877F2]">Send message</button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
